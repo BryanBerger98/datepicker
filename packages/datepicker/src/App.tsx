30e2fc2d76c1f34d81fe3dc27637a1334fd70ef2
Bryan BERGER <contact@weberger.net>
2023-10-13 21:05:32 +0200

    docs: update readme.md (#29)

--- packages/datepicker/src/App.tsx
+++ packages/datepicker/src/App.tsx
@@ -8,36 +8,40 @@ const App = () => {
 
 	return (
 		<>
-			<div className="flex flex-col gap-8 p-8">
-				<h1 className="text-2xl font-bold">Datepicker</h1>
-				<div className="flex flex-col gap-4">
-					<h2 className="text-xl font-semibold">Simple calendar</h2>
-					<div className="flex">
-						<SimpleCalendarDemo />
+			<div className="flex flex-col gap-8 p-8 w-screen">
+				<h1 className="text-2xl font-bold text-center">Datepicker</h1>
+				<div className="flex gap-8 mx-auto flex-wrap">
+					<div className="flex flex-col gap-4">
+						<h2 className="text-xl font-semibold">Simple calendar</h2>
+						<div className="flex">
+							<SimpleCalendarDemo />
+						</div>
+					</div>
+					<div className="flex flex-col gap-4">
+						<h2 className="text-xl font-semibold">Multi months calendar</h2>
+						<div className="flex">
+							<MultiMonthsCalendarDemo />
+						</div>
 					</div>
 				</div>
-				<div className="flex flex-col gap-4">
-					<h2 className="text-xl font-semibold">Multi months calendar</h2>
-					<div className="flex">
-						<MultiMonthsCalendarDemo />
+				<div className="flex flex-wrap mx-auto gap-8">
+					<div className="flex flex-col gap-4">
+						<h2 className="text-xl font-semibold">Multi dates selection calendar</h2>
+						<div className="flex">
+							<MultiSelectCalendarDemo />
+						</div>
 					</div>
-				</div>
-				<div className="flex flex-col gap-4">
-					<h2 className="text-xl font-semibold">Multi dates selection calendar</h2>
-					<div className="flex">
-						<MultiSelectCalendarDemo />
+					<div className="flex flex-col gap-4">
+						<h2 className="text-xl font-semibold">Range selection calendar</h2>
+						<div className="flex">
+							<RangeCalendarDemo />
+						</div>
 					</div>
-				</div>
-				<div className="flex flex-col gap-4">
-					<h2 className="text-xl font-semibold">Range selection calendar</h2>
-					<div className="flex">
-						<RangeCalendarDemo />
-					</div>
-				</div>
-				<div className="flex flex-col gap-4">
-					<h2 className="text-xl font-semibold">Disabled calendar</h2>
-					<div className="flex">
-						<DisabledCalendarDemo />
+					<div className="flex flex-col gap-4">
+						<h2 className="text-xl font-semibold">Disabled calendar</h2>
+						<div className="flex">
+							<DisabledCalendarDemo />
+						</div>
 					</div>
 				</div>
 			</div>
