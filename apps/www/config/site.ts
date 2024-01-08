c196483034e09a0ac0e37521ea76afc5ff7056e6
Bryan BERGER <contact@weberger.net>
2024-01-08 09:04:44 +0100

    docs: update package description

--- apps/www/config/site.ts
+++ apps/www/config/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
 	name: 'Datepicker',
 	url: 'https://datepicker.bryanberger.dev',
 	ogImage: 'https://datepicker.bryanberger.dev/og.jpg',
-	description: 'React composable date picker inspired by react-day-picker.',
+	description: 'Flexible date picker for React. Composable. Customizable. Open Source.',
 	links: {
 		threads: 'https://www.threads.net/@bryanberger.dev',
 		github: 'https://github.com/BryanBerger98/datepicker',
